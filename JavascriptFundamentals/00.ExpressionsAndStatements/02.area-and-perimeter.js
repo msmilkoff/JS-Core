d0acd4f2222d9e18242b3946468681dbcc1e5993
msmilkoff <msmilkoff@gmail.com>
2016-12-29 16:00:18 +0200

    Solve 'hello, js' and 'area and perimeter'

---
+++ JavascriptFundamentals/00.ExpressionsAndStatements/02.area-and-perimeter.js
@@ -0,0 +1,10 @@
+function solve([a, b]) {
+    let width = Number(a);
+    let height = Number(b);
+
+    let area = width * height;
+    let perimeter = (2 * width) + (2 * height);
+
+    console.log(area);
+    console.log(perimeter);
+}
